ca3665d60dc878ba2e381fab82289221af27a31a
Shehu Muhammad <shehuaamuhammad@gmail.com>
2026-01-05 23:05:56 -0500

    installed bootstrap and imported it, also added components to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Button from './components/Button';
+import Input from './components/Input';
+import List from './components/List';
+import Message from './components/Message';
+import Title from './components/Title';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const gameInstructions = [
+    'Enter the range of numbers you want the target number to be between, and press New Target',
+    'Enter your guess',
+    'See if your guess is too high or too low',
+    'Change your guess',
+    'The app will tell you when you are right, and how many guesses you had',
+  ];
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Title tag='h1' title='Number Guessing Game' />
+      <Message message='Guess a number' />
+      <Input />
+      <Input />
+      <Button name='New Target' />
+      <Message message='The number is greater than X' />
+      <Input />
+      <Button name='Guess' />
+      <Title tag='h3' title='Instructions' />
+      <List instructions={gameInstructions} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
